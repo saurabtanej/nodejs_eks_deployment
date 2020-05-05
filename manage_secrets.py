a543d9db0267e1f9e5b63b2f6f72d7c07ea59736
saurabtanej <staneja.st@gmail.com>
2020-05-05 20:12:22 +0400

    adding ansible encryption

--- manage_secrets.py
+++ manage_secrets.py
@@ -2,14 +2,25 @@
 """
 Update Kubernetes secret file
 
+Requirement:
+Python >~ 3
+Ansible ~> 2
+"ansible-vault" password file placed in home directory of the user executing the script named "ansible-vault"
+
 Usage
-./k8s_secrets.py --key <secret_key> --value <secret_value> --service <service_name> [--existing]
+./manage_secrets.py --key <secret_key> --value <secret_value> --service <service_name> [--existing]
 
 How it works
 1. If its a new service, it will create a secrets.yaml with secrets (key: value(in base64)) file in the service location
 2. If the service already exist, and "--existing" is passed, it will check:
     a. if the secret already exist, if yes update the encdid value.
     b. if the secret is new, add the secret in secret.yaml file
+3. Do the ansible encyption/decryption on the fly.
+
+NOTE: Save "ansible-vault" password in the home location of current user.
+
+To Do:
+Add encyption/decryption utility from .gitarrtributes using git-crypt
 
 """
 
@@ -62,9 +73,12 @@ if __name__ == "__main__":
     ENCODEDVALUE = base64.b64encode(args.value.encode('ascii'))
 
     if args.existing and os.path.isfile(FILE):
+        os.system("ansible-vault decrypt --vault-password-file ~/ansible-vault {}".format(FILE))
         update_secrets(args.key, ENCODEDVALUE.decode('ascii'), FILE)
+        os.system("ansible-vault encrypt --vault-password-file ~/ansible-vault {}".format(FILE))
     else:
         if os.path.isdir("helm/charts/" + args.service):
             create_secrets(args.key, ENCODEDVALUE.decode('ascii'), args.service, FILE)
+            os.system("ansible-vault encrypt --vault-password-file ~/ansible-vault {}".format(FILE))
         else:
             print("Helm chart for %s does not exist" % args.service)
